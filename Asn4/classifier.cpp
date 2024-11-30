8c5d3bb6e0c40dbe457d4511e846c66bc2250824
ishaq-ameer <inasiru3540@conestogac.on.ca>
2024-11-29 21:01:40 -0500

    Updated predict second feature

---
+++ Asn4/classifier.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+void train() {
+    printf("train\n");
+    printf("The first feature is added\n")
+}
+
+void predict() {
+    printf("predict\n");
+    printf("The second feature is added\n");
+}
